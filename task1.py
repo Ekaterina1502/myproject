a4b69aca2533a2d5d33894130cec4cafa8e69cda
Ekaterina1502 <74763433+Ekaterina1502@users.noreply.github.com>
2020-12-20 22:21:05 +1000

    Revert "последнееДЗурок8"

--- task1.py
+++
@@ -1,9 +0,0 @@
-my_f = open("text.rft","w")
-
-while True:
-    my_var = input ('Введите что-нибудь')
-    if my_var == '':
-        break
-    my_f.write(f'{my_var}\n')
-
-my_f.close()
--- упр 2.py
+++
@@ -1,7 +0,0 @@
-my_file = open ("для задания2.txt","r")
-lines = 0
-
-for line in my_file:
-    lines+=1
-    print(f' Количество слов в строке {lines}:{len(line.split())}')
-print (f'Количество строк в файле - {lines}')
